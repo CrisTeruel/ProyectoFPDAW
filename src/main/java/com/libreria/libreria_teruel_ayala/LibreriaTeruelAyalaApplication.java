fb96557b593c1fbf7e2640343f2b7aff3e78f950
CrisTeruel <crisay1991@gmail.com>
2026-04-20 17:02:42 +0200

    cargamos el springIO
    generamos el springIO

---
+++ src/main/java/com/libreria/libreria_teruel_ayala/LibreriaTeruelAyalaApplication.java
@@ -0,0 +1,13 @@
+package com.libreria.libreria_teruel_ayala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaTeruelAyalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaTeruelAyalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libreria/libreria_teruel_ayala/LibreriaTeruelAyalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libreria.libreria_teruel_ayala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaTeruelAyalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
