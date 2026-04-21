8167f0bc16a0b8ce3b114fc1085bf918f1742495
CrisTeruel <crisay1991@gmail.com>
2026-04-21 08:41:20 +0200

    jpa editorial
     hecha

--- src/main/java/com/libreria/libreria_teruel_ayala/model/Editorial.java
+++ src/main/java/com/libreria/libreria_teruel_ayala/model/Editorial.java
@@ -1,4 +1,22 @@
 package com.libreria.libreria_teruel_ayala.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "editorial")
 public class Editorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 120)
+    private String nombre;
+
+    // una editorial tiene muchos libros
+    @OneToMany(mappedBy = "editorial")
+    private List<Libro> libros;
 }
