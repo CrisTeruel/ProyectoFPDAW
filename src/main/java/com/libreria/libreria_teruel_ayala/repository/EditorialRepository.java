3be91d937ae9f88c737e31f0c10851b79f0721a5
CrisTeruel <crisay1991@gmail.com>
2026-04-22 09:41:04 +0200

    bug en editorial interfaz
    me deje poner q implementa el metodo de findbyNombre

--- src/main/java/com/libreria/libreria_teruel_ayala/repository/EditorialRepository.java
+++ src/main/java/com/libreria/libreria_teruel_ayala/repository/EditorialRepository.java
@@ -3,6 +3,8 @@ package com.libreria.libreria_teruel_ayala.repository;
 import com.libreria.libreria_teruel_ayala.model.Editorial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// Autogenerado Spring
 public interface EditorialRepository extends JpaRepository<Editorial, Integer> {
+
+    // buscar editorial por nombre para no duplicar
+    Editorial findByNombre(String nombre);
 }
