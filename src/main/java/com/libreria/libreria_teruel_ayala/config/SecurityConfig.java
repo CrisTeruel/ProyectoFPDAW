5c391cf0fbffd91955da394a53fde0dcf27f0492
CrisTeruel <crisay1991@gmail.com>
2026-05-26 09:05:14 +0200

    lio con favicon
    metido favicon

--- src/main/java/com/libreria/libreria_teruel_ayala/config/SecurityConfig.java
+++ src/main/java/com/libreria/libreria_teruel_ayala/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/busqueda", "/libro/**", "/img/**", "/css/**").permitAll()
+                        .requestMatchers("/", "/busqueda", "/libro/**", "/img/**", "/css/**", "/favicon.ico").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
